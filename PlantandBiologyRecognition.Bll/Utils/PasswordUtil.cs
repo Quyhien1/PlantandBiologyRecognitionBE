5b562daca44e1fae21e6dc9cb952a99e61cae7b9
Hien <hiennpqse180072@fpt.edu.vn>
2025-06-09 09:24:14 +0700

    using method instead of calling BCrypt directly

--- PlantandBiologyRecognition.Bll/Utils/PasswordUtil.cs
+++ PlantandBiologyRecognition.Bll/Utils/PasswordUtil.cs
@@ -10,14 +10,14 @@ namespace PlantandBiologyRecognition.BLL.Utils
     public static class PasswordUtil
     {
         private const int HashingRound = 10;
-        public static async Task<string> HashPassword(string rawPassword)
+        public static string HashPassword(string rawPassword)
         {
-            return await Task.Run(() => BCrypt.Net.BCrypt.HashPassword(rawPassword, workFactor: HashingRound));
+            return BCrypt.Net.BCrypt.HashPassword(rawPassword, workFactor: HashingRound);
         }
 
-        public static async Task<bool> VerifyPassword(string rawPassword, string hashedPassword)
+        public static bool VerifyPassword(string rawPassword, string hashedPassword)
         {
-            return await Task.Run(() => BCrypt.Net.BCrypt.Verify(rawPassword, hashedPassword));
+            return BCrypt.Net.BCrypt.Verify(rawPassword, hashedPassword);
         }
     }
 }
